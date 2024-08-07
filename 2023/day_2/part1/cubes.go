a5c4f08ebfad122d5d2dafe6b25ee28ad963f0c3
Alexander Markelov <alex1111859@gmail.com>
2024-08-07 23:40:51 +0300

    finished day 2 part 1

---
+++ 2023/day_2/part1/cubes.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("../Input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	sum := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		result := isGameValid(line)
+		if result == true {
+			sum += getGameId(line)
+		}
+	}
+	fmt.Println(sum)
+}
+
+func getGameId(input string) int {
+	regexpGameId, _ := regexp.Compile("[0-9]+")
+	GameByte := regexpGameId.Find([]byte(input))
+	GameId, _ := strconv.Atoi(string(GameByte))
+	return GameId
+}
+
+func isGameValid(line string) bool {
+	regexpListOfCubes, _ := regexp.Compile("([0-9]+ (blue|green|red)){1}")
+	listOfCubes := regexpListOfCubes.FindAll([]byte(line), -1)
+	for _, token := range listOfCubes {
+		number := getNumber(token)
+		color := getColor(token)
+		result := isResultValid(number, color)
+		if result == false {
+			return false
+		}
+	}
+	return true
+}
+
+func getNumber(token []byte) int {
+	regexprNumber, _ := regexp.Compile("[0-9]+")
+	numberByte := regexprNumber.Find([]byte(token))
+	number, _ := strconv.Atoi(string(numberByte))
+	return number
+}
+
+func getColor(token []byte) string {
+	regexprColor, _ := regexp.Compile("(blue|green|red)")
+	color := regexprColor.Find([]byte(token))
+	return string(color)
+}
+
+func isResultValid(number int, color string) bool {
+	result := []struct {
+		amount int
+		color string
+	}{
+		{12, "red"},
+		{13, "green"},
+		{14, "blue"},
+	}
+	for _, res := range result {
+		if color == res.color {
+			if number > res.amount {
+				return false
+			}
+		}
+	}
+	return true
+}
