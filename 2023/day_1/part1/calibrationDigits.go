e2ab60901b7d305e490177ef49776e1e90dbfe5c
Alexander Markelov <alex1111859@gmail.com>
2024-08-10 23:17:46 +0300

    added returning index from a digit searching functions

--- 2023/day_1/part1/calibrationDigits.go
+++ 2023/day_1/part1/calibrationDigits.go
@@ -3,34 +3,35 @@ package calibrationDigits
 import (
 	"errors"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
-func FindFirstDigit(documentLine string) (int, error) {
+func FindFirstDigit(documentLine string) (int, int, error) {
 	for _, char := range documentLine {
 		if unicode.IsDigit(char) {
 			result, _ := strconv.Atoi(string(char))
-			return result, nil // TODO возвращать ещё и индекс?
+			return result, strings.Index(documentLine, string(char)), nil
 		}
 	}
-	return 0, errors.New("No digit found")
+	return 0, 0, errors.New("No digit found")
 }
 
-func FindSecondDigit(documentLine string) (int, error) {
+func FindSecondDigit(documentLine string) (int, int, error) {
 	for i := len(documentLine) - 1; i >= 0; i-- {
 		if unicode.IsDigit(rune(documentLine[i])) {
 			result, _ := strconv.Atoi(string(documentLine[i]))
-			return result, nil
+			return result, strings.Index(documentLine, string(documentLine[i])), nil
 		}
 	}
-	return 0, errors.New("No digit found")
+	return 0, 0, errors.New("No digit found")
 }
 
 func GetCalibrationSum(document []string) int {
 	var sum = 0
 	for _, str := range document {
-		num1, _ := FindFirstDigit(str)
-		num2, _ := FindSecondDigit(str)
+		num1, _,  _ := FindFirstDigit(str)
+		num2, _, _ := FindSecondDigit(str)
 		sum += num1*10 + num2
 	}
 	return sum
