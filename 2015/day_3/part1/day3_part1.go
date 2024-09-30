dc72dc24c7da43af6ae5b771368eb3db791991a4
Alexander Markelov <alex1111859@gmail.com>
2024-09-30 19:44:26 +0300

    finished 2015 day 3 part 1

---
+++ 2015/day_3/part1/day3_part1.go
@@ -0,0 +1,62 @@
+package day3_part1
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+type Coord struct {
+	x int
+	y int
+}
+
+func CountHouses(filename string) (total int) {
+	houses := make([]Coord, 100)
+	houses = append(houses, Coord{0, 0})
+	total = 1
+	var x, y int
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		ch, _, err := reader.ReadRune()
+		if err != nil {
+			break
+		}
+		switch(ch) {
+		case '>':
+			x++
+		case '<':
+			x--
+		case '^':
+			y++
+		case 'v':
+			y--
+
+		}
+		newHouse := Coord{x, y}
+		if !ContainsHouse(newHouse, houses) {
+			houses = append(houses, newHouse)
+			total += 1
+		}
+		
+	}
+	return total
+}
+
+func ContainsHouse(coord Coord, houses []Coord) bool {
+	for _, val := range houses {
+		if CompareStructs(val, coord) {
+			return true
+		}
+	}
+	return false
+}
+
+func CompareStructs(val, coord Coord) bool {
+	return (val.x == coord.x) && (val.y == coord.y)
+}
---
+++ 2015/day_3/part1/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"gitlab.com/alexmarkelov42/AdventOfCode/2015/day_3/part1"
+	"fmt"
+)
+
+func main() {
+	result := day3_part1.CountHouses("../../input.txt")
+	fmt.Println(result)
+}
