8f08d04f1d33929f73f72074a22e3d800bbed2f0
Alexander Markelov <alex1111859@gmail.com>
2024-09-30 18:39:28 +0300

    finished 2015 day 2 part 1

---
+++ 2015/day_2/part1/day2_part1.go
@@ -0,0 +1,35 @@
+package day2_part1
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func TotalSquareFeet(filename string) (total int) {
+	var (
+		l = 0
+		w = 0
+		h = 0
+	)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Sscanf(str, "%dx%dx%d", &l, &w, &h)
+		num1 := l * w
+		num2 := l * h
+		num3 := w * h
+		square := 2 * num1 + 2 * num2 + 2 * num3 + min(num1, num2, num3)
+		total += square
+	}
+	return total
+}
---
+++ 2015/day_2/part1/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"gitlab.com/alexmarkelov42/AdventOfCode/2015/day_2/part1"
+	"fmt"
+)
+
+func main() {
+	result := day2_part1.TotalSquareFeet("../../input.txt")
+	fmt.Println(result)
+}
