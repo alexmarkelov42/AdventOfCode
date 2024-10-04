127e29dee575fa711c3ac9c3bc81646bf58ff2aa
Alexander Markelov <alex1111859@gmail.com>
2024-10-04 15:18:36 +0300

    finished 2015 day 7 part 2

---
+++ 2015/day_7/part2/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	day7_part2 "gitlab.com/alexmarkelov42/AdventOfCode/2015/day_7/part2"
+)
+
+func main() {
+	result := day7_part2.RunLogicGates("../../input.txt")
+	fmt.Println(result)
+}
