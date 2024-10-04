c25197ba599b30dc7e7a23c1b5ba27bd10b02706
Alexander Markelov <alex1111859@gmail.com>
2024-10-04 17:54:36 +0300

    changed solution to strconv.quote

--- 2015/day_8/part1/day_8_part1.go
+++ 2015/day_8/part1/day_8_part1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
 	"unicode"
 )
 
@@ -16,29 +17,28 @@ func CountChars(filename string) (total int) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		data := scanner.Text()
-		num1 := len(data)
-		num2 := countActualChars(data)
-		total = total + num1 - num2
+		qstr, _ := strconv.Unquote(data)
+		total = total + len(data) - len(qstr)
 	}
 	return total
 }
 
 func countActualChars(data string) (result int) {
 	var ch rune
-	for i := 1; i < len(data) - 1; i++ {
+	for i := 1; i < len(data)-1; i++ {
 		ch = rune(data[i])
 		if unicode.IsLetter(ch) {
 			result++
 			continue
 		}
-		if ch == '\\' && (data[i+1] == '\\' || data[i+1] == '"') { // 
+		if ch == '\\' && (data[i+1] == '\\' || data[i+1] == '"') {
 			result++
 			i++
 			continue
 		}
 		if ch == '\\' && data[i+1] == 'x' {
 			result++
-			i+=3
+			i += 3
 			continue
 		}
 	}
