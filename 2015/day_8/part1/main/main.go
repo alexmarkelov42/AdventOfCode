569446c193da33fccdabb113871f1ffc11905b56
Alexander Markelov <alex1111859@gmail.com>
2024-10-04 17:32:16 +0300

    finished 2015 day 8 part 1

---
+++ 2015/day_8/part1/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	day_8_part1 "gitlab.com/alexmarkelov42/AdventOfCode/2015/day_8/part1"
+)
+
+func main() {
+	result := day_8_part1.CountChars("../../input.txt")
+	fmt.Println(result)
+}
