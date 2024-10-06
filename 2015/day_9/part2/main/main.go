edc315ce67c5760680b9edd8d00dc96c2fffe655
Alexander Markelov <alex1111859@gmail.com>
2024-10-06 19:01:32 +0300

    finished 2015 day 9 part 2

---
+++ 2015/day_9/part2/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	day_9_part2 "gitlab.com/alexmarkelov42/AdventOfCode/2015/day_9/part2"
+)
+
+func main() {
+	result := day_9_part2.LongestRoute("../../input.txt")
+	fmt.Println(result)
+}
