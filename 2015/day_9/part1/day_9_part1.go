4170b8258d54eda0bb65fea7e057d35e81e8f41b
Alexander Markelov <alex1111859@gmail.com>
2024-10-06 19:01:16 +0300

    finished 2015 day 9

---
+++ 2015/day_9/part1/day_9_part1.go
@@ -0,0 +1,92 @@
+package day_9_part1
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const N int = 8
+
+var shortestPath int
+
+func ShortestRoute(filename string) int {
+	data := readArray(filename)
+	shortestPath := findShortestPath(data)
+	return shortestPath
+}
+
+func findShortestPath(data [][]int) (total int) {
+	arr := make([]int, N)
+	for i := range N {
+		arr[i] = i
+	}
+	total = allPermutations(arr, data, N, N, total)
+	return total
+}
+
+func allPermutations(arr []int, data [][]int, size, n, total int) (result int) {
+	if size == 1 {
+		res := pathLength(arr, data)
+		if res < total || total == 0 {
+			total = res
+		}
+		return total
+	}
+	for i := range(N) {
+		total = allPermutations(arr, data, size - 1, n, total)
+		if size % 2 == 1 {
+			arr[0], arr[size-1] = arr[size-1], arr[0]
+		} else {
+			arr[i], arr[size-1] = arr[size-1], arr[i]
+		}
+	}
+	return total
+}
+
+func pathLength(arr []int, data [][]int) (result int) {
+	for i := 1; i < len(arr); i++ {
+		i1 := arr[i-1]
+		i2 := arr[i]
+		result += data[i1][i2]
+	}
+	return result
+}
+
+func readArray(filename string) (arr [][]int) {
+	data := make([][]int, N)
+	for i := range N {
+		data[i] = make([]int, N)
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for i := range N {
+		for j := i + 1; j < N; j++ {
+			scanner.Scan()
+			str := strings.Split(scanner.Text(), " = ")
+			data[i][j], _ = strconv.Atoi(str[1])
+			data[j][i] = data[i][j]
+		}
+	}
+	return data
+}
+
+func findMinInd(arr []int, path map[int]bool) (minEl, ind int) {
+	minEl = slices.Max(arr)
+	for i := 0; i < len(arr); i++ {
+		if !path[i] {
+			if arr[i] != 0 && arr[i] <= minEl {
+				minEl = arr[i]
+				ind = i
+			}
+		}
+	}
+	return minEl, ind
+}
---
+++ 2015/day_9/part1/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	day_9_part1 "gitlab.com/alexmarkelov42/AdventOfCode/2015/day_9/part1"
+)
+
+func main() {
+	result := day_9_part1.ShortestRoute("../../input.txt")
+	fmt.Println(result)
+}
